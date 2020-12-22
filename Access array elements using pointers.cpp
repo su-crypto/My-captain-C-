2bacdb5bb8234e876d14717195102c205572a721
su-crypto <55615349+su-crypto@users.noreply.github.com>
2020-12-22 16:49:06 +0530

    Add files via upload
    C++ program to access the elements of an Array using pointers.

---
+++ Access array elements using pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int data[5];
+   cout << "Enter elements: ";
+
+   for(int i = 0; i < 5; ++i)
+      cin >> data[i];
+
+   cout << "You entered: ";
+   for(int i = 0; i < 5; ++i)
+      cout << "\n"<< *(data + i);
+	return 0;
+}
